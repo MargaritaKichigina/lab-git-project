c619741cd21cddfce82344d829031b4e4baa183a
Margarita <rita.suslenok@mail.ru>
2025-01-26 18:05:45 +0300

    добавлены файлы

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from utils import add_numbers, multiply_numbers
+
+num1 = 5
+num2 = 10
+
+sum_result = add_numbers(num1, num2)
+product_result = multiply_numbers(num1, num2)
+
+print(f'Сумма {num1} и {num2} равна: {sum_result}')
+print(f'Произведение {num1} и {num2} равно: {product_result}')
