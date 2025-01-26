890b6b7a4ef4362ccbd79a8891ee3bea5bc7ee51
Margarita <rita.suslenok@mail.ru>
2025-01-26 18:55:31 +0300

    добавлена третья переменная для умножения чисел

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b, c):
-    return a + b + c
+def add_numbers(a, b):
+    return a + b 
 
-def multiply_numbers(a, b):
-    return a * b
+def multiply_numbers(a, b, c):
+    return a * b * c
